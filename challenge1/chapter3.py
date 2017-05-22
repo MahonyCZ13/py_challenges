30f119b0bfe44db1fa3ec9a6701252bfb36d015e
Petr Maronek <maronek.petr@tutamail.com>
2017-05-22 14:10:58 +0200

    chapter 3 completed

--- challenge1/chapter3.py
+++ challenge1/chapter3.py
@@ -1,5 +1,17 @@
 ### Chapter 3 Challenge ###
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [11, 21, 2, 3, 5, 8, 13, 21, 34, 55, 89, 1, 5, 4, 2]
 
-print(a)
+a2 = []
+
+for i in a:
+    if i < 5:
+        a2.append(i)
+
+print(sorted(a2))
+
+### Extras ###
+
+num = int(input('I will list numbers less than: '))
+a3 = [i for i in a if i < num]
+print(a3)
