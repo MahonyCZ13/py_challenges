0241a01c8e4bbe5ea776cee8dd194a21ef8a16aa
Petr Maronek <maronek.petr@tutamail.com>
2017-05-22 14:22:59 +0200

    chapter 4 completed

--- challenge1/chapter4.py
+++ challenge1/chapter4.py
@@ -1,5 +1,19 @@
 ### Chapter 4 Challenge ###
 
+# Create list
 
+num = int(input('Give me a number: '))
+
+a = range(1, 100)
+
+div_list = []
+
+for i in a:
+    if i % num == 0:
+        div_list.append(i)
+
+print(div_list)
 
 ### Extras ###
+
+# No extras for this challenge
