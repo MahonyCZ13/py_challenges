61b0fe9bb15c994ce5374e50efd5a30474cb4edf
Petr Maronek <maronek.petr@gmail.com>
2017-05-22 15:20:21 +0200

    chapter 5 completed

--- challenge1/chapter5.py
+++ challenge1/chapter5.py
@@ -1,5 +1,19 @@
 ### Chapter 4 Challenge ###
 
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# this method only works in equaly sized lits
+result = [i for i, j in zip(a, b) if i ==j]
+print(result)
 
 ### Extras ###
 
+import random
+
+c = random.sample(range(1, 100), 15)
+d = random.sample(range(1, 100), 20)
+
+
+
+print(set(c) & set(d))
