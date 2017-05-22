89e87a156f47c607486f84233ec44b7d56321c56
Petr Maronek <maronek.petr@tutamail.com>
2017-05-22 13:40:04 +0200

    chapter 2 completed

--- challenge1/chapter2.py
+++ challenge1/chapter2.py
@@ -1,2 +1,20 @@
 ### Chapter 2 Challenge ###
 
+user_number = int(input('Give me a number: '))
+
+if(user_number % 4 == 0):
+    print('The number is even and is multiple of 4.')
+elif(user_number % 2 == 0):
+    print('The number is even.')
+else:
+    print('The number is odd')
+
+### Extras ###
+
+num = int(input('Give me a first number: '))
+check = int(input('Give me a second number: '))
+
+if(check % num == 0):
+    print('The number %s divides evenly into %s.' % (check, num))
+else:
+    print('The numbers you have provided don\'t return even result.')
