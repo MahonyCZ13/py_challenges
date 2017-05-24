bf708e3b37015589dea2eebdf1263bf60cfd4327
Petr Maronek <maronek.petr@gmail.com>
2017-05-24 16:08:06 +0200

    Chapter 9 completed

--- challenge1/chapter9.py
+++ challenge1/chapter9.py
@@ -1,10 +1,30 @@
 ### Chapter 9 Challenge ###
 
+import random
+
+i = 1
+running = True
+
+while running:
+    i += 1
+    user  = input('Guess a number from 1 to 5! (or type \'q\' to quit): ')
+    if(user == 'q'):
+        print('You have played %s games.' % i)
+        running = False
+    else:
+        user = int(user)
+        gen = random.randint(1,5)
+
+        if(user == gen):
+            print('Yes!! It\'s a match! The computer thought %s and you guessed %s.' % (gen, user))
+        else:
+            print('Sadly, wrong. Computer thought %s and you guessed %s.' % (gen, user))
 
 
 ### Extras ###
 
-# No extras for this challenge
+# Count how many games user played. - Done
+# Type 'q' to quit the game otherwise continue playing. - Done
 
 ### Quicker example ###
 
