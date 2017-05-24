5e61b4a7cfd9fa896fced9de0351a67c87efa268
Petr Maronek <maronek.petr@gmail.com>
2017-05-24 11:53:12 +0200

    added scipt flow based on user input (start/quit)

--- challenge1/chapter8.py
+++ challenge1/chapter8.py
@@ -1,5 +1,21 @@
 ### Chapter 8 Challenge ###
 
+while True:
+    usr_cmd = input('Type \'start\' to start the game or \'quit\' to end the game > ')
+    if(usr_cmd == 'quit'):
+        break
+    elif(usr_cmd == 'start'):
+        user1 = input('Player 1: Rock paper or scissors?: ')
+        user2 = input('Player 2: Rock paper or scissors?: ')
+        # print('These are the inputs from Player 1: %s and Player 2: %s.' % (user1, user2))
+
+        
+    else:
+        print('This is a bad command.')
+
+
+
+
 
 
 ### Extras ###
