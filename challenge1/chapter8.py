20e1bf6146246e29b3b3870d75ccfd6b2780deb4
Petr Maronek <maronek.petr@gmail.com>
2017-05-24 13:21:11 +0200

    Chapter 8 completed

--- challenge1/chapter8.py
+++ challenge1/chapter8.py
@@ -5,18 +5,24 @@ while True:
     if(usr_cmd == 'quit'):
         break
     elif(usr_cmd == 'start'):
-        user1 = input('Player 1: Rock paper or scissors?: ')
-        user2 = input('Player 2: Rock paper or scissors?: ')
+        user1 = int(input('Player 1: Rock paper or scissors?: '))
+        user2 = int(input('Player 2: Rock paper or scissors?: '))
         # print('These are the inputs from Player 1: %s and Player 2: %s.' % (user1, user2))
 
-        
+        result = [" ", "rock", "paper", "scissors"]
+
+        if(user1 == user2):
+            print('It\'s a tie!')
+        elif(user1 == 3 and user2 == 1):
+            print('Player 2 picked %s and won!' % result[user2])
+        elif(user1 == 1 and user2 == 2):
+            print('Player 2 picked %s and won!' % result[user2])
+        elif(user1 == 2 and user2 == 3):
+            print('Player 2 picked %s and won!' % result[user2])
+        else:
+            print('Player 1 picked %s and won!' % result[user1])
     else:
         print('This is a bad command.')
-
-
-
-
-
 
 ### Extras ###
 
@@ -24,4 +30,9 @@ while True:
 
 ### Quicker example ###
 
-# No quicker examples for this challenge
+'''if(user1 == user2):
+    print('It\'s a tie!')
+elif((user1 == 3 and user2 == 1) or (user1 == 1 and user2 == 2) or (user1 == 2 and user2 == 3)):
+    print('Player 2 picked %s and won!' % result[user2])
+else:
+    print('Player 1 picked %s and won!' % result[user1])'''
