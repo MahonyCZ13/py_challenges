319fabad05d5ea810b5cabbb1e7e0f3c79ae9438
Petr Maronek <maronek.petr@gmail.com>
2017-05-25 15:38:07 +0200

    fixed pylint notifications

--- challenge1/chapter10.py
+++ challenge1/chapter10.py
@@ -1,23 +1,25 @@
 ### Chapter 10 Challenge ###
+'''
+Compares two lists for the same values.
+'''
+import random
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+A = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+B = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-print(set(a) & set(b))
+print(set(A) & set(B))
 
 ### Extras ###
 
 # Generate two lists and compare them
 
-import random
-
-c = random.sample(range(1, 50), 15)
-d = random.sample(range(1, 50), 15)
+C = random.sample(range(1, 50), 15)
+D = random.sample(range(1, 50), 15)
 
 # Another way of solving this without sets
-result = [i for i in c if i in d]
+RESULT = [i for i in C if i in D]
 
-print(result)
+print(RESULT)
 
 ### Quicker example ###
 
