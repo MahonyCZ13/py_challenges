24427ce20b286d419d7b32e6c8766d5c48705c7a
Petr Maronek <maronek.petr@gmail.com>
2017-05-25 13:20:38 +0200

    Chapter 11 completed

--- challenge1/chapter11.py
+++ challenge1/chapter11.py
@@ -1,6 +1,28 @@
 ### Chapter 11 Challenge ###
 
+import math
 
+def get_number():
+    user_num = int(input('Give me a number > '))
+    return user_num
+
+def prime_checker(num):
+
+    list_div = []
+    i = 1
+    
+    while i <= (math.sqrt(num)):
+        if(num % i == 0):
+            list_div.append(i)
+        i += 1
+    
+    if(len(list_div) == 1):
+        print('This is a prime number!')
+    else:
+        print('This is not a prime number, because it has %s divisors.' % (len(list_div) * 2))
+             
+prime_checker(get_number())
+    
 
 ### Extras ###
 
