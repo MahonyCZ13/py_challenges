f0dcaa61f43c329b1ec22242c3c0f0ff404432f6
Petr Maronek <maronek.petr@gmail.com>
2017-05-25 16:04:46 +0200

    Chapter 12 created

---
+++ challenge1/chapter12.py
@@ -0,0 +1,13 @@
+### Chapter 7 Challenge ###
+'''
+Takes list and create a new one of only first and a last elemets.
+'''
+
+
+### Extras ###
+
+# No extras for this challenge
+
+### Quicker example ###
+
+# No quicker examples for this challenge
