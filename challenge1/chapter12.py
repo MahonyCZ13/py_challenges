264389ded6405dd9bbd00ff680b45d8be22c52b4
Petr Maronek <maronek.petr@gmail.com>
2017-05-25 16:24:37 +0200

    Chapter 12 completed

--- challenge1/chapter12.py
+++ challenge1/chapter12.py
@@ -2,7 +2,26 @@
 '''
 Takes list and create a new one of only first and a last elemets.
 '''
+def create_list():
+    '''() -> list
 
+    Creates list from a list from only the first and the last elements.
+
+    >>> create_list()
+    [1, 2, 3, 4] -> [1, 4]
+    >>> create_list()
+    [3, 1, 6, 3, 1] -> [3, 1]
+
+    '''
+    base_list = [5, 10, 15, 20, 25]
+    new_list = []
+
+    new_list.insert(0, base_list.pop(0))
+    new_list.append(base_list.pop())
+
+    print('%s -> %s' % (base_list, new_list))
+
+create_list()
 
 ### Extras ###
 
