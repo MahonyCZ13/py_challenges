53005cf960d2ffb30dc7350cda652bc9af2494e9
Petr Maronek <maronek.petr@gmail.com>
2017-05-29 10:25:34 +0200

    Chapter 14 created

---
+++ challenge1/chapter14.py
@@ -0,0 +1,14 @@
+### Chapter 14 Challenge ###
+'''
+Takes a list with miltiple values a returns a list with these values minus the duplicates.
+'''
+
+
+
+### Extras ###
+
+# No extras for this challenge
+
+### Quicker example ###
+
+# No quicker examples for this challenge
