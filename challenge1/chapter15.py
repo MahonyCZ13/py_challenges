dbf73c21e717d68dc1076011f0ff3400b81ea2e6
Petr Maronek <maronek.petr@gmail.com>
2017-05-29 17:07:33 +0200

    Chapter 15 created

---
+++ challenge1/chapter15.py
@@ -0,0 +1,14 @@
+### Chapter 15 Challenge ###
+'''
+Reverse word order.
+'''
+
+
+
+### Extras ###
+
+# No extras for this challenge
+
+### Quicker example ###
+
+# No quicker examples for this challenge
