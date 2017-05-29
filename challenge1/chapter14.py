1b4cf8b01a27273adc54c94e3265418924627634
Petr Maronek <maronek.petr@gmail.com>
2017-05-29 13:49:55 +0200

    Chapter 14 completed

--- challenge1/chapter14.py
+++ challenge1/chapter14.py
@@ -3,12 +3,60 @@
 Takes a list with miltiple values a returns a list with these values minus the duplicates.
 '''
 
+def trim_list():
+    '''(list) -> list
+
+    Creates new list without the duplicates in the supplied list.
+
+    >>> trim_list([1, 1, 2, 3, 4, 4, 5])
+    [1, 2, 3, 4, 5]
+    >>> trim_list([1, 2, 2])
+    [1, 2]
+    '''
+    list_one = [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6, 6, 6, 7]
+    list_two = []
+
+    i = 1
+
+    while i <= len(list_one):
+        if i in list_one:
+            list_two.append(i)
+        i += 1
+
+    print(list_two)
+
+trim_list()
 
 
 ### Extras ###
 
-# No extras for this challenge
+## Using sets
+
+def trim_set():
+    '''(set) -> set
+
+    Same as trim_list but usig sets.
+    '''
+    set_one = set([1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6, 6, 6, 7])
+
+    result = list(set(set_one))
+
+    print(result)
+
+trim_set()
 
 ### Quicker example ###
 
-# No quicker examples for this challenge
+def trim_quick():
+    '''(list) - > list
+    Same as trim_list but usig 'not in' condition.
+    '''
+    list_three = [1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 6, 6, 6, 7, 7]
+    list_four = []
+
+    for i in list_three:
+        if i not in list_four:
+            list_four.append(i)
+    print(list_four)
+
+trim_quick()
