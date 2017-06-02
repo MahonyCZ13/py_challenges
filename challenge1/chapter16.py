4b3f5516ba255474c2bb28dce786e9d24d289218
Petr Maronek <maronek.petr@gmail.com>
2017-06-02 10:59:21 +0200

    Chapter 16 created

---
+++ challenge1/chapter16.py
@@ -0,0 +1,14 @@
+### Chapter 16 Challenge ###
+'''
+Password generator.
+'''
+
+
+
+### Extras ###
+
+# No extras for this challenge
+
+### Quicker example ###
+
+# No quicker examples for this challenge
