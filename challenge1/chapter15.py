b6d514b4960e303526cf2845d61678a08ae0dc06
Petr Maronek <maronek.petr@gmail.com>
2017-06-02 10:51:36 +0200

    Chapter 15 completed

--- challenge1/chapter15.py
+++ challenge1/chapter15.py
@@ -14,8 +14,13 @@ def string_reverse():
     ? alien you Are
     '''
     user = input('Give me a string and i will reverse it > ')
-    result = str.split(user)
-    print(result)
+    result = user.split()
+    finish = []
+    for i in reversed(result):
+        finish.append(i)
+
+    print(' '.join(finish))
+
 string_reverse()
 
 ### Extras ###
@@ -24,4 +29,16 @@ string_reverse()
 
 ### Quicker example ###
 
-# No quicker examples for this challenge
+def reverse_word(word):
+    '''(str) -> str
+
+    Reverses word order inputed by user.
+
+    >>> string_reverse('Hello there user')
+    user there Hello
+    >>> string('Are you alien?')
+    ? alien you Are
+    '''
+    print(' '.join(word.split()[::-1]))
+
+reverse_word('Hello there user')
