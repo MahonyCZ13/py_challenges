09e1ccada79ff4039b1e4555d3fd01712561ca84
Petr Maronek <maronek.petr@outlook.com>
2018-11-22 10:24:15 +0100

    challenge 3 completed

--- python_challenge/screen3.py
+++ python_challenge/screen3.py
@@ -1,3 +1,13 @@
 ##############################
 # Challenge 3
 ##############################
+
+import re
+
+body = open("bodyguards.txt", "r")
+
+r = re.findall('[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+', body.read())
+m = ''.join(r)
+
+if m is not None:
+    print (m)
